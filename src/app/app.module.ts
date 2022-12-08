dfcc798679ac83c94bf175b3ed2d82805f2d0396
malwozniak <mgrztwnk5@gmail.com>
2022-12-08 12:36:36 +0100

    2 files modified, 8 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ListComponent } from '../list/list.component';
-import { RandomComponent } from './random/random.component';
-import { RandomService } from './random/random.service';
-import {
-  MatCardModule, MatListModule, MatButtonModule, MatInputModule, MatExpansionModule, MatToolbarModule, MatIconModule, MatSnackBarModule, MatSlideToggleModule, MatChipsModule
-} from '@angular/material';
+import { ListComponent } from './components/list/list.component';
+import { RandomComponent } from './components/random/random.component';
+import { RandomService } from './components/random/random.service';
+import { ListService } from './components/list/list.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatCardModule,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
     MatListModule,
     MatButtonModule,
     MatInputModule,
@@ -20,9 +32,10 @@ import {
     MatIconModule,
     MatSnackBarModule,
     MatSlideToggleModule,
-    MatChipsModule],
+    MatChipsModule,
+  ],
   declarations: [AppComponent, HelloComponent, ListComponent, RandomComponent],
   bootstrap: [AppComponent],
-  providers: [RandomService],
+  providers: [RandomService, ListService],
 })
 export class AppModule {}
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from './list.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  constructor(private service: ListService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
