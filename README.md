f593398c3adb000ffe03eb5cdee89cf1761f985a
malwozniak <mgrztwnk5@gmail.com>
2022-12-08 10:47:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-random-list
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-8hdwsh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  @Input() mojePole: string = 'Gosia';
+ 
+
+}
