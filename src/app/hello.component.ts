99d3873ba1ef3808923ba23aa0f3ec66d4c06bb7
malwozniak <mgrztwnk5@gmail.com>
2022-12-08 18:00:32 +0100

    4 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1> {{mojePole}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`],
 })
 export class HelloComponent {
   @Input() mojePole: string;
